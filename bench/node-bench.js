08556d3221b15d16b6eaa8d328778f9b8fde367c
Peter Burns <rictic@google.com>
2024-10-11 15:01:44 -0700

    Compare against old version, and JSON.parse

--- bench/node-bench.js
+++ bench/node-bench.js
@@ -4,6 +4,7 @@
 
 import { streamJsonParser } from "./stream-json.js";
 import { parse } from "./bundles/bundle.min.js";
+import { parse as unoptimizedParse } from "./bundles/baseline.min.js";
 import * as fs from "node:fs";
 import * as path from "node:path";
 
@@ -31,17 +32,44 @@ async function jsonParse(jsonString) {
   }
   return finalValue;
 }
+async function jsonParseOld(jsonString) {
+  let finalValue;
+  for await (const val of unoptimizedParse(toStream(jsonString))) {
+    finalValue = val;
+  }
+  return finalValue;
+}
 
-async function benchmarkFile(str, name, times) {
-  const streamJsonTimes = [];
-  const jsonRiverTimes = [];
-  for (let i = 0; i < times; i++) {
-    const start = performance.now();
-    await jsonParse(str);
-    jsonRiverTimes.push(performance.now() - start);
-    const start2 = performance.now();
-    await streamJsonParser(str);
-    streamJsonTimes.push(performance.now() - start2);
+const comparisons = [
+  {
+    name: "jsonriver",
+    parse: jsonParse,
+  },
+  {
+    name: "jsonriver v0.1",
+    parse: jsonParseOld,
+  },
+  {
+    name: "stream-json",
+    parse: streamJsonParser,
+  },
+  {
+    name: "JSON.parse",
+    parse: JSON.parse,
+  },
+];
+
+async function benchmarkFile(comparisons, str, name, numTimes) {
+  const times = [];
+  for (const comparison of comparisons) {
+    times.push([]);
+  }
+  for (let i = 0; i < numTimes; i++) {
+    for (let j = 0; j < comparisons.length; j++) {
+      const start = performance.now();
+      await comparisons[j].parse(str);
+      times[j].push(performance.now() - start);
+    }
   }
 
   // Report mean and standard deviation
@@ -52,20 +80,22 @@ async function benchmarkFile(str, name, times) {
     const m = mean(arr);
     return Math.sqrt(arr.reduce((a, b) => a + (b - m) ** 2, 0) / arr.length);
   }
-  console.log(`Stats from parsing ${name} averaged over ${times} runs`);
-  console.log(
-    `  jsonriver       ${mean(jsonRiverTimes)
-      .toFixed(3)
-      .padStart(10, " ")}ms ±${stdDev(jsonRiverTimes).toFixed(2)}ms`
-  );
-  console.log(
-    `  stream-json     ${mean(streamJsonTimes)
-      .toFixed(3)
-      .padStart(10, " ")}ms ±${stdDev(streamJsonTimes).toFixed(2)}ms`
-  );
+  console.log(`Parsing ${name} averaged over ${numTimes} runs`);
+  for (let i = 0; i < comparisons.length; i++) {
+    console.log(
+      `  ${comparisons[i].name.padEnd(15, " ")} ${mean(times[i])
+        .toFixed(3)
+        .padStart(10, " ")}ms ±${stdDev(times[i]).toFixed(2)}ms`
+    );
+  }
   console.log("\n");
 }
 
-await benchmarkFile(smallJsonString, "a small file (64KiB)", 100);
-await benchmarkFile(mediumJsonString, "a medium file (1.4MiB)", 100);
-await benchmarkFile(largeJsonString, "a large file (25MiB)", 3);
+await benchmarkFile(comparisons, smallJsonString, "a small file (64KiB)", 100);
+await benchmarkFile(
+  comparisons,
+  mediumJsonString,
+  "a medium file (1.4MiB)",
+  100
+);
+await benchmarkFile(comparisons, largeJsonString, "a large file (25MiB)", 3);
